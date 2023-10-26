0049d0fbcbccc3f4d35ab72a0b578aa4fbcceccb
cl-kim <nias0327@naver.com>
2023-10-26 16:35:36 +0900

    test: Add test cases for MemberService methods

---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,102 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long memberId = memberService.join(member);
+
+        //Then
+        Member findMember = memberService.findOne(memberId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+
+        //Then
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+        /* 이렇게 할 수도 있지만 assertThrows를 사용하면 된다.
+        try {
+            memberService.join(member2);
+            fail();
+        }catch (IllegalStateException e){
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        }
+         */
+
+    }
+
+    @Test
+    void 전체_회원_조회() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring1");
+        memberService.join(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        memberService.join(member2);
+
+        //when
+        List<Member> result = memberService.findMembers();
+
+        //Then
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+    @Test
+    void 단일_회원_조회() {
+        //given
+        Member member = new Member();
+        member.setName("spring1");
+        Long memberId = memberService.join(member);
+
+        //when
+        Member result = memberService.findOne(memberId).get();
+
+        //Then
+        assertThat(result).isEqualTo(member);
+    }
+}
